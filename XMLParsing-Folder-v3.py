47ad352628aba93787a69f9799749176e31f117e
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-06-08 20:46:10 +0530

    Update XMLParsing-Folder-v3.py

--- XMLParsing-Folder-v3.py
+++ XMLParsing-Folder-v3.py
@@ -21,26 +21,39 @@ directory=r'C:\Users\KZ623JK\OneDrive - EY\Documents\Carrier'
 obj=os.scandir(directory)
 
 effective_date = input('enter the effectivedate')
-print(effective_date)
+input_manuscriptid = input('enter the manuscript id')
+input_versionid = input('enter the version id')
+input_lob = input('enter the lob')
+input_caption = input('enter the caption')
+
+#print(effective_date)
+
 
 for entry in obj:
     for file in os.scandir(entry):
         parser=ET.XMLParser(encoding='utf-8')
         root=ET.parse(file.path,parser=parser)
-        print('manuscriptID',root.getroot()[0].attrib['manuscriptID'])
-        print('versionDate',root.getroot()[0].attrib['versionDate'])
-        #print(root.getroot()[0][0][3].attrib['name'])
+        #print('manuscriptID',root.getroot()[0].attrib['manuscriptID'])
+        #print('versionDate',root.getroot()[0].attrib['versionDate'])
+        root.getroot()[0].attrib['versionDate']=effective_date
+        root.getroot()[0].attrib['manuscriptID']=input_manuscriptid
+        root.getroot()[0].attrib['versionID'] = input_versionid
+        root.getroot()[0].attrib['lob'] = input_lob
+        root.getroot()[0].attrib['caption'] = input_caption
+            
         
         myroot=root.getroot()[0]
-        
         for x in myroot:
             if x.tag=='keys':
                 for y in x:
+                    if y.attrib['name']=='lob':
+                        y.attrib['value']=input_lob
                     if y.attrib['name']=='effectiveDateNew':
                         y.attrib['value']=effective_date
-                        print(y.attrib['value'])
+                        #print(y.attrib['value'])
                     if y.attrib['name']=='effectiveDateRenewal':
-                        print(y.attrib)
+                        y.attrib['value']=effective_date
+                        
                         
         root.write(file.path)
         print('done')
